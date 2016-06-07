0f72f4a839124f0f31b44f6ef254b4b80aceb03b
Damon <damon@mahuika.co.nz>
2016-06-07 19:31:22 +1200

    mockInitModules return a function for us in test.mockAppContext

--- src/mocks/module.js
+++ src/mocks/module.js
@@ -2,26 +2,27 @@ import { MiniMongo } from './minimongo'
 import { MockApiRegistry } from './api'
 
 
-export const mockInitModules = (...contextAndModules) => {
-  const meteorCoreContext = {
-    Meteor: {
-      isServer: true,
-    },
-    Tracker: {},
-    Mongo: MiniMongo,
-    Users: new MiniMongo.Collection('users'),
-    SimpleSchema: global.SimpleSchema,
-    apiRegistry: new MockApiRegistry(),
+export const mockInitModules = (...contextAndModules) =>
+  () => {
+    const meteorCoreContext = {
+      Meteor: {
+        isServer: true,
+      },
+      Tracker: {},
+      Mongo: MiniMongo,
+      Users: new MiniMongo.Collection('users'),
+      SimpleSchema: global.SimpleSchema,
+      apiRegistry: new MockApiRegistry(),
+    }
+    const passedContext = typeof contextAndModules[0] !== 'function'
+    const [context, ...modules] = passedContext ? contextAndModules : [{}, ...contextAndModules]
+    const resultContext = {
+      ...meteorCoreContext,
+      ...context,
+    }
+    modules.forEach(module => {
+      const moduleContext = module(resultContext)
+      Object.assign(resultContext, moduleContext)
+    })
+    return resultContext
   }
-  const passedContext = typeof contextAndModules[0] !== 'function'
-  const [context, ...modules] = passedContext ? contextAndModules : [{}, ...contextAndModules]
-  const resultContext = {
-    ...meteorCoreContext,
-    ...context,
-  }
-  modules.forEach(module => {
-    const moduleContext = module(resultContext)
-    Object.assign(resultContext, moduleContext)
-  })
-  return resultContext
-}
