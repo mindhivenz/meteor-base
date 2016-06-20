f7e390bc49e8eb0e6ffd0e48063d52c0cdd8fb6e
Damon <damon@mahuika.co.nz>
2016-06-21 11:41:21 +1200

    Blow up if registering enhancements after enhancing

--- src/enhancer.js
+++ src/enhancer.js
@@ -5,9 +5,13 @@ export class Enhancer {
   constructor() {
     this.apiContextEnhanceObj = { enhanced: true }
     this.apiContextEnhanceFuncs = []
+    this.haveEnhanced = false
   }
 
   registerEnhancement(objOrFunc) {
+    if (this.haveEnhanced) {
+      throw new Error("Can't register new enhancements after enhancing")
+    }
     if (typeof objOrFunc === 'function') {
       this.apiContextEnhanceFuncs.push(objOrFunc)
     } else {
@@ -19,6 +23,7 @@ export class Enhancer {
     if (typeof target.enhanced === 'undefined') {
       Object.assign(target, this.apiContextEnhanceObj)
       this.apiContextEnhanceFuncs.forEach(f => f(target))
+      this.haveEnhanced = true
     }
   }
 }
