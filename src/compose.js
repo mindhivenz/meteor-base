e5ab42dd466b5374162c9ac7a5ae1ba2893e2304
Damon <damon@mahuika.co.nz>
2016-07-17 21:16:03 +1200

    Add shouldResubscribe to withReactiveData

--- src/compose.js
+++ src/compose.js
@@ -4,6 +4,12 @@ import { inject } from '@mindhive/di'
 
 const Empty = () => null
 
+/*
+ asyncFunc: (appContext, pushProps, ownProps)
+
+ Call pushProps with the props to push to the child component.
+ You must call pushProps when asyncFunc is first called.
+ */
 export const withAsync = (asyncFunc, shouldResubscribe) =>
   compose(
     inject((appContext, ownProps, onData) => {
@@ -17,22 +23,21 @@ export const withAsync = (asyncFunc, shouldResubscribe) =>
   )
 
 /*
- Inside meteorDataUsingFunc calls to Meteor reactive calls are tracked,
- and meteorDataUsingFunc rerun if the result of those Meteor calls changes
-
- meteorDataUsingFunc: (appContext, pushProps, ownProps)
-
- Call pushProps with the props to push to the child component.
+ As per withAsync but calls to Meteor reactive calls inside asyncFunc are tracked,
+ and asyncFunc rerun if the result of those Meteor calls changes.
+ This rerun is not blocked by shouldResubscribe.
 
  Note: we don't use the loading and error component of react-komposer.
  Push that data through props to handle it nicely.
  */
-export const withReactiveData = (meteorDataUsingFunc) =>
+export const withReactiveData = (asyncFunc, shouldResubscribe) =>
   composeWithTracker(
     inject((appContext, ownProps, onData) => {
       const pushProps = (props = {}) =>
         onData(null, props)
-      meteorDataUsingFunc(appContext, pushProps, ownProps)
+      asyncFunc(appContext, pushProps, ownProps)
     }),
     Empty,
+    null,
+    { shouldResubscribe },
   )
