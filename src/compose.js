a10113fe3a85e0cef32537eacfdbd3d2746391b2
Damon <damon@mahuika.co.nz>
2016-09-29 12:01:35 +0900

    Refactor compose and warn of mis-use

--- src/compose.js
+++ src/compose.js
@@ -2,22 +2,30 @@ import { compose, composeWithTracker } from 'react-komposer'
 import { inject } from '@mindhive/di'
 
 
-const Empty = () => null
+const PushPropsNotCalled = () => {
+  console.error("You didn't call pushProps")  // eslint-disable-line no-console
+}
+
+const composeFunc = (asyncFunc) =>
+  inject((appContext, ownProps, onData) => {
+    const pushProps = (props = {}) =>
+      onData(null, props)
+    asyncFunc(appContext, pushProps, ownProps)
+  })
 
 /*
  asyncFunc: (appContext, pushProps, ownProps)
 
  Call pushProps with the props to push to the child component.
  You must call pushProps when asyncFunc is first called.
+
+ Note: we don't use the loading and error component of react-komposer.
+ Push that data through props to handle it nicely.
  */
 export const withAsync = (asyncFunc, shouldResubscribe) =>
   compose(
-    inject((appContext, ownProps, onData) => {
-      const pushProps = (props = {}) =>
-        onData(null, props)
-      asyncFunc(appContext, pushProps, ownProps)
-    }),
-    Empty,
+    composeFunc(asyncFunc),
+    PushPropsNotCalled,
     null,
     { shouldResubscribe },
   )
@@ -26,18 +34,11 @@ export const withAsync = (asyncFunc, shouldResubscribe) =>
  As per withAsync but calls to Meteor reactive calls inside asyncFunc are tracked,
  and asyncFunc rerun if the result of those Meteor calls changes.
  This rerun is not blocked by shouldResubscribe.
-
- Note: we don't use the loading and error component of react-komposer.
- Push that data through props to handle it nicely.
  */
 export const withReactiveData = (asyncFunc, shouldResubscribe) =>
   composeWithTracker(
-    inject((appContext, ownProps, onData) => {
-      const pushProps = (props = {}) =>
-        onData(null, props)
-      asyncFunc(appContext, pushProps, ownProps)
-    }),
-    Empty,
+    composeFunc(asyncFunc),
+    PushPropsNotCalled,
     null,
     { shouldResubscribe },
   )
