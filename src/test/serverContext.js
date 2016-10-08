40db7ce5c77b3d60861bfaac1a583a6493324786
Damon <damon@mahuika.co.nz>
2016-10-08 10:10:23 +0900

    Running in fiber should return result

--- src/test/serverContext.js
+++ src/test/serverContext.js
@@ -1,12 +1,17 @@
 import { mockAppContext } from '@mindhive/di/test'
 
 
-const inFiberTestRunner = (func) =>
-  (done) => {
-    func.future()().resolve((err) => {
-      done(err)
+const inFiberAsPromise = (func) =>
+  () =>
+    new Promise((resolve, reject) => {
+      func.future()().resolve((err, result) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(result)
+        }
+      })
     })
-  }
 
 export const mockServerContext = (testModules, testFunc) =>
-  inFiberTestRunner(mockAppContext(testModules, testFunc))
+  inFiberAsPromise(mockAppContext(testModules, testFunc))
