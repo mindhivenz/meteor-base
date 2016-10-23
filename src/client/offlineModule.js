d25998991849d727e396d7fad8f39b3931342446
Damon <damon@mahuika.co.nz>
2016-10-23 22:51:49 +0900

    Allow attachSchema with GroundDB

--- src/client/offlineModule.js
+++ src/client/offlineModule.js
@@ -1,11 +1,17 @@
 import { Ground } from 'meteor/ground:db'  // eslint-disable-line import/no-unresolved
 
 
-export default ({ Meteor, Tracker }) => {
+export default ({ Meteor, Tracker, Mongo }) => {
   if (! Meteor.isProduction) {
     if (! Tracker.pumpMongoToMobx) {
       console.warn('space:tracker-mobx-autorun package does not appear to be installed yet you want offline')  // eslint-disable-line no-console
     }
+  }
+  Ground.Collection.prototype.attachSchema = Mongo.Collection.prototype.attachSchema
+  if (! Ground.Collection.prototype.deny) {
+    // Otherwise attachSchema doesn't work, but what functionality are we losing with attachSchema using deby?
+    // https://github.com/aldeed/meteor-collection2-core/blob/1f4acc68a50a4b62793a327ed34832bd9254998a/lib/collection2.js#L547
+    Ground.Collection.prototype.deny = () => {}
   }
   return {
     offlineEnabled: true,
