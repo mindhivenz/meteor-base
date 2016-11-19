79d65121bf82b6ddb10e09048fb23c5ce6a85b84
Damon <damon@mahuika.co.nz>
2016-11-19 12:10:59 +0900

    withDomain

--- domain/index.js
+++ domain/index.js
@@ -1,6 +1,9 @@
 var lookup = require('./../dist/domain/lookup')
+var withDomain = require('./../dist/domain/withDomain')
 
 module.exports = {
   Lookup: lookup.Lookup,
   LookupDomain: lookup.LookupDomain,
+
+  withDomain: withDomain.withDomain,
 }
---
+++ src/domain/withDomain.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import shallowEqual from 'shallowequal'
+
+
+export const withDomain = ({
+  domainClass,
+  mapPropsToArg,
+  mapPropsToArgsArray = props => mapPropsToArg ? [mapPropsToArg(props)] : [],
+  createDomain = props => new domainClass(...mapPropsToArgsArray(props)),  // eslint-disable-line new-cap
+  propName = 'domain',
+  shouldRecreateDomain = (currentProps, nextProps) =>
+    ! shallowEqual(mapPropsToArgsArray(currentProps), mapPropsToArgsArray(nextProps)),
+}) =>
+  Component =>
+    class DomainProvider extends React.Component {
+
+      domain
+
+      componentWillMount() {
+        this.domain = createDomain(this.props)
+      }
+
+      componentWillUpdate(nextProps) {
+        if (shouldRecreateDomain(this.props, nextProps)) {
+          this.stop()
+          this.domain = createDomain(nextProps)
+        }
+      }
+
+      componentWillUnmount() {
+        this.stop()
+      }
+
+      stop() {
+        if (this.domain && typeof this.domain.stop === 'function') {
+          this.domain.stop()
+        }
+      }
+
+      render() {
+        return React.createElement(Component, { ...this.props, [propName]: this.domain })
+      }
+    }
