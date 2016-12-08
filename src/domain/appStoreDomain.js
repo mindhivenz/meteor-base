51bef88b7e50615632de5f4887e82321b97919b2
Damon <damon@mahuika.co.nz>
2016-12-08 18:55:24 +0900

    appStoreDomain.js

---
+++ src/domain/appStoreDomain.js
@@ -0,0 +1,58 @@
+import {
+  observable,
+  computed,
+  runInAction,
+} from 'mobx'
+import { app } from '@mindhive/di'
+
+/* eslint-disable no-console */
+
+const device = global.device
+const getAppVersion = global.cordova.getAppVersion
+const WebAppLocalServer = global.WebAppLocalServer
+
+class AppStoreDomain {
+
+  @observable packageName
+  @observable needsUpdate = false
+
+  constructor() {
+    if (app().Meteor.isCordova) {
+      if (! device) {
+        throw new Error('You need to install the cordova:cordova-plugin-device')
+      }
+      if (! getAppVersion) {
+        throw new Error('You need to install the cordova:cordova-plugin-app-version')
+      }
+      getAppVersion.getPackageName()
+        .then((packageName) => {
+          runInAction('set packageName', () => { this.packageName = packageName })
+        })
+      WebAppLocalServer.onError((e) => {
+        if (String(e).includes('Cordova platform or versions changed')) {
+          runInAction('set needsUpdate', () => { this.needsUpdate = true })
+        }
+      })
+    }
+  }
+
+  @computed get storeUrl() {
+    switch (device.platform) {
+      case 'Android': {
+        if (! this.packageName) {
+          console.warn('Returning null storeUrl as packageName not retrieved yet')
+          return null
+        }
+        return `https://play.google.com/store/apps/details?id=${this.packageName}`
+      }
+      default: {
+        console.warn('Returning null storeUrl un unsupported platform')
+        return null
+      }
+    }
+  }
+}
+
+export default () => ({
+  appStoreDomain: new AppStoreDomain(),
+})
