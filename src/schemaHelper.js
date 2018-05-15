0b9583ddf7335f8994bfd1ffd0ccddf59361a0a0
Damon <damon@mahuika.co.nz>
2018-05-15 13:53:53 +1200

    Handle new simpl-schema way of storing fields

--- src/schemaHelper.js
+++ src/schemaHelper.js
@@ -1,12 +1,13 @@
 import uniq from 'lodash/uniq'
 
 
-export const topLevelFieldsFromSchema = schemaOrMongoFieldsDef =>
-  schemaOrMongoFieldsDef &&
-    uniq(
-      Object.keys(schemaOrMongoFieldsDef)
-        .map(f => f.split('.')[0])
-    )
+export const topLevelFieldsFromSchema = (schemaOrMongoFieldsDef) => {
+  if (! schemaOrMongoFieldsDef) {
+    return null
+  }
+  const deepFields = schemaOrMongoFieldsDef._schemaKeys || Object.keys(schemaOrMongoFieldsDef)
+  return uniq(deepFields.map(f => f.split('.')[0]))
+}
 
 export const collectionAttachedSchema = collection =>
   collection._c2 && collection._c2._simpleSchema && collection._c2._simpleSchema.mergedSchema()
